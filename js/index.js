add9795c814b1a1be7bf54aa702afbd302bb2d91
weixiao <1206055855@qq.com>
2022-06-12 17:08:30 +0800

    添加侧边栏

---
+++ js/index.js
@@ -0,0 +1,22 @@
+//侧边栏
+var is_ce=false;
+var ce_an=$('.ce_an');
+var ce=$('.ce');
+
+
+ce_an.click(function(){
+	// alert('123')
+	if(!is_ce){
+		ce.css({
+			'left':'0',
+			'opacity':'1'
+		})
+		is_ce=true;
+	}else{
+		ce.css({
+			'left':'-300px',
+			'opacity':'1'
+		})
+		is_ce=false;
+	}
+})
